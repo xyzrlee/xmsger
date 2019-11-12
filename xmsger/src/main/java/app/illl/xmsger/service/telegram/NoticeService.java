9079a6d1a5cfc8aaa50eb01be0eda3175f336b7b
Ricky Li <cnrickylee@gmail.com>
2019-11-12 09:24:07 +0800

    add afternoon notice (#38)

---
+++ xmsger/src/main/java/app/illl/xmsger/service/telegram/NoticeService.java
@@ -0,0 +1,7 @@
+package app.illl.xmsger.service.telegram;
+
+public interface NoticeService {
+
+    int monthlyNotice(Integer chatId, int daysInAdvance);
+
+}
--- xmsger/src/main/java/app/illl/xmsger/service/telegram/NoticeServiceImpl.java
+++ xmsger/src/main/java/app/illl/xmsger/service/telegram/NoticeServiceImpl.java
@@ -1,13 +1,9 @@
-package app.illl.xmsger.schedule;
+package app.illl.xmsger.service.telegram;
 
 import app.illl.xmsger.datasource.entity.NoticeMonthly;
 import app.illl.xmsger.datasource.service.NoticeMonthlyService;
-import app.illl.xmsger.datasource.service.TelegramRegisteredChatService;
-import app.illl.xmsger.service.telegram.SendMessageService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -15,27 +11,16 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-@Component
 @RequiredArgsConstructor
-@Slf4j
-class NoticeMonthlyScheduler {
-
-    private static final int NOTICE_DAYS = 3;
+@Component
+public class NoticeServiceImpl implements NoticeService {
 
     private final NoticeMonthlyService noticeMonthlyService;
-    private final TelegramRegisteredChatService telegramRegisteredChatService;
     private final SendMessageService sendMessageService;
 
-    @Scheduled(cron = "0 0 8 * * *")
-    void notice() {
-        List<Integer> chatIds = telegramRegisteredChatService.getAllChatId();
-        if (null == chatIds) return;
-        for (Integer chatId : chatIds) {
-            notice(chatId, NOTICE_DAYS);
-        }
-    }
-
-    private void notice(Integer chatId, @SuppressWarnings("SameParameterValue") int daysInAdvance) {
+    @Override
+    public int monthlyNotice(Integer chatId, int daysInAdvance) {
+        int messageSent = 0;
         LocalDate date = LocalDate.now();
         String dateMsg = "today";
         for (int i = 0; i < daysInAdvance; i++) {
@@ -59,9 +44,10 @@ class NoticeMonthlyScheduler {
             if (!noticeMessageList.isEmpty()) {
                 String message = "[" + dateMsg + "] " + StringUtils.join(noticeMessageList, ", ");
                 sendMessageService.sendPlainText(chatId, message);
+                messageSent++;
             }
             date = date.plusDays(1);
         }
+        return messageSent;
     }
-
 }
